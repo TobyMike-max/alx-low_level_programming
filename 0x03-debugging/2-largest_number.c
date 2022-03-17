5558c54847aac7c620b50f5c2c0ecafd2b5fc96d
TobyMike-max <osinubi.michael@gmail.com>
2022-03-17 05:12:49 -0700

    added brackets to comparison code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,18 +5,18 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
+ * Return: Always largest number
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if ((a > b) && (b >= c))
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if ((b >= a) && (b >= c))
 	{
 		largest = b;
 	}
