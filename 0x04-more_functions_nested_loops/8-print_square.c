ecd8ce0e989671a8b1f8440e0eeb834a54120a93
TobyMike-max <osinubi.michael@gmail.com>
2022-03-17 18:59:24 -0700

    Corrected sourcecode 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (htg = 0; hgt < size; hgt++)
+		for (hgt = 0; hgt < size; hgt++)
 		{
 			for (wid = 0; wid < size; wid++)
 				_putchar('#');
