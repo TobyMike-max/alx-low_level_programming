d796314731906b66f7ddf598f52c9dfbf17e6e44
TobyMike-max <osinubi.michael@gmail.com>
2022-03-21 18:20:13 -0700

    Had to satisfy betty in 2,3,4 & 5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,7 +5,7 @@
  */
 void _puts(char *str)
 {
-	while(*str)
+	while (*str)
 		_putchar(*str++);
 
 	_putchar('\n');
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,11 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0, index = 0;
 
-	while(s[index++])
+	while (s[index++])
 		len++;
-	for(index = len - 1; index >= 0; index--)
+	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
