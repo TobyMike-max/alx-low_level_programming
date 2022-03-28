0479b6b508b3320c1b1b1436de4da88478ddc730
TobyMike-max <osinubi.michael@gmail.com>
2022-03-28 13:32:12 -0700

    Push Task 8 to github

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,7 +4,7 @@
  * print_diagsums - prints the sum of the two
  * diagonals of a square matrix of integers
  * @a: input pointer.
- * @size: size of the matrix
+ * @size: size of te matrix
  * Return: no return.
  */
 void print_diagsums(int *a, int size)
