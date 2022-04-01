bebafe72de43284c4c2e9eb06b69577ac3da09d0
TobyMike-max <osinubi.michael@gmail.com>
2022-04-01 12:09:55 -0700

    Upload the first file to github

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints its name, followed by a new line.
+ * @agrc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
