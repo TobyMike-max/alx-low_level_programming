a269af0ff5c7c77c4a9c9f7a30b858bde0d340b9
TobyMike-max <osinubi.michael@gmail.com>
2022-04-07 13:07:34 -0700

    Push task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ * Return: If new_size == old_size - ptr.
+ * If new_size == 0 and ptr is not NULL - NULL.
+ * Otherwise - a pointer to the reallocated memory block.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *memory;
+	char *ptr_copy, *filler;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		memory = malloc(new_size);
+
+		if (memory == NULL)
+			return (NULL);
+
+		return (memory);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr_copy = ptr;
+	memory = malloc(sizeof(*ptr_copy) * new_size);
+
+	if (memory == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	filler = memory;
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		filler[i] = *ptr_copy++;
+
+	free(ptr);
+	return (memory);
+}
