f46fe604725e3fce6a6955493f7a90b950332737
TobyMike-max <osinubi.michael@gmail.com>
2022-04-13 10:27:13 -0700

    Removed trailing white space

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function given as 
+ * array_iterator - Executes a function given as
  * a parameter on each element of an array.
  * @array: The array.
  * @size: The size of array.
  * @action: A pointer to the function to be executed
  * Return: void.
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
