97508f6a66858b3c2a1a95a0a9d2fcd4395b4de5
TobyMike-max <osinubi.michael@gmail.com>
2022-04-13 17:33:32 -0700

    Included curly braces to wrap function statements

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -13,7 +13,10 @@ int op_mod(int a, int b);
  * Return: The sum of a and b.
  */
 int op_add(int a, int b)
+{
 	return (a + b);
+}
+
 
 /**
  * op_sub - Returns the difference of two numbers.
@@ -22,7 +25,10 @@ int op_add(int a, int b)
  * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
+{
 	return (a - b);
+}
+
 
 /**
  * op_mul - Returns the product of two numbers.
@@ -31,7 +37,10 @@ int op_sub(int a, int b)
  * Return: The product of a and b.
  */
 int op_mul(int a, int b)
+{
 	return (a * b);
+}
+
 
 /**
  * op_div - Returns the division of two numbers.
@@ -40,7 +49,10 @@ int op_mul(int a, int b)
  * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
+{
 	return (a / b);
+}
+
 
 /**
  * op_mod - Returns the remainder of the division of two numbers
@@ -49,5 +61,6 @@ int op_div(int a, int b)
  * Return: The remainder of the division of a and b.
  */
 int op_mod(int a, int b)
+{
 	return (a % b);
-
+}
