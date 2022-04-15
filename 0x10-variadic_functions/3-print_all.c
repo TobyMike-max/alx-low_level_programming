318ce70a0df90f2a38d2117cfe5230bc16106870
TobyMike-max <osinubi.michael@gmail.com>
2022-04-14 20:53:31 -0700

    Removed space in function prototype declaration

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@ void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
-void print_all (const char * const format, ...);
+void print_all(const char * const format, ...);
 
 /**
  * print_car - Prints a char.
