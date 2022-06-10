a4647dc64e742852dd29ab65677d3b265efee68b
TobyMike-max <osinubi.michael@gmail.com>
2022-06-10 06:00:01 +0100

    [Create]: Password for crackme5 file

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Generates and prints passwords for the crackme5 executable
+ * @argc: Number of argument count supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
+
+	codex = "A-C
+}
