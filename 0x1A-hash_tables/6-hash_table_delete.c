ecf621f4e1437f7febcbfd1c7d8ace6affb459f8
TobyMike-max <osinubi.michael@gmail.com>
2022-07-02 13:45:49 +0100

    [Create]: Function to delete a hash node

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: pointer to a hash table type.
+ *
+ * Return: void.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *current, *next;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			current = ht->array[i];
+			while (current)
+			{
+				next = current->next
+				free(current->key);
+				free(current->value);
+				free(current);
+				current = next;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
