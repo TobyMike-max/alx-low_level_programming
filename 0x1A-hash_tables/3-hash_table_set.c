09dfb944a827a94f5b465edaaa9b697cb5741c19
TobyMike-max <osinubi.michael@gmail.com>
2022-07-02 09:45:51 +0100

    [Modified]: ht NULL checker

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,6 +25,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (ht == NULL)
+		return (0);
+
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
