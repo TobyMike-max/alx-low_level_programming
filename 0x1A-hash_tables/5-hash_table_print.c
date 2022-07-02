1f40f1c45673c5b46a5cfb2a40b31d4f25c09bde
TobyMike-max <osinubi.michael@gmail.com>
2022-07-02 13:10:04 +0100

    [Modified]: Corrected printf function spelling

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			if (comma_flag == 1)
-				print(", ");
+				printf(", ");
 
 			current = ht->array[i];
 			while (current)
