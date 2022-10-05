8375643d885ad6db837f7c058437f019a7d4d7fa
TobyMike-max <osinubi.michael@gmail.com>
2022-10-05 07:56:19 +0100

    [Create]: Linear search algo for task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search - Function that searches for a value in an array
+ * @array - Pointer to the first element of the array to search in
+ * @size - The number of elements in array
+ * @value - The value to search for
+ * Return: -1 - For missing value or NULL array
+ * Otherwise - First index where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	
+	if (array == NULL)
+		return (-1);
+
+	for(i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
